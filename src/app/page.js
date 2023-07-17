cf520e7005bac5064d62ee14aa201f57b73a9357
nikhil <nikhilbhagat2293@gmail.com>
2023-07-17 18:41:21 +0530

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default async function Home() {
   return (
     <>
       <div className="my-5 px-3 grid items-center gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
-        {mov.data.movies.map((movie) => (
+        {mov?.data?.movies?.map((movie) => (
           <Card movie={movie} key={movie.id} />
         ))}
       </div>
