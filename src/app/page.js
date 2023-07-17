2d444eda76f94f3f7f62287457a6a9e0be173870
nikhil <nikhilbhagat2293@gmail.com>
2023-07-17 15:31:09 +0530

    added movie detail page

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
 import Card from "@/components/Card";
 
 async function getData() {
-  const res = await fetch("https://yts.mx/api/v2/list_movies.json");
+  const res = await fetch("https://yts.mx/api/v2/list_movies.json", {
+    cache: "no-cache",
+  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  const data = await res.json();
 
-  return res.json();
+  return data;
 }
 
 export default async function Home() {
   const mov = await getData();
-  console.log(mov.data.movies[0]);
   return (
     <>
-      <div className="my-5 px-3 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+      <div className="my-5 px-3 grid items-center gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
         {mov.data.movies.map((movie) => (
           <Card movie={movie} key={movie.id} />
         ))}
--- src/app/search/[id]/page.jsx
+++ src/app/search/[id]/page.jsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 async function getData(id) {
   const res = await fetch(
-    `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    `https://yts.mx/api/v2/movie_details.json?movie_id=${id}&with_cast=true`,
+    { cache: "no-cache" }
   );
 
   return res.json();
@@ -12,15 +13,46 @@ const page = async ({ params }) => {
   const single = await getData(params.id);
   console.log(single);
   return (
-    <div>
-      <Image
-        width={230}
-        height={345}
-        src={single.data.movie.medium_cover_image}
-      />
-      <h3>{single.data.movie.title}</h3>
-      <h4>{single.data.movie.rating}</h4>
-      <p>{single.data.movie.description_intro}</p>
+    <div className="flex flex-col gap-8 md:flex-row text-white">
+      <div className="w-full md:w-[26%]">
+        <img src={single.data.movie.medium_cover_image} alt="poster" />
+      </div>
+      <div className="w-full md:max-w-2xl flex  flex-col gap-4">
+        <h3 className="text-white text-4xl">{single.data.movie.title}</h3>
+        <h4>
+          <span className="mr-2 font-bold text-xl">Rating :</span>
+          {single.data.movie.rating}
+        </h4>
+        <p>
+          <span className="mr-2 font-bold text-xl">plot :</span>
+          {single.data.movie.description_intro}
+        </p>
+        <div className="flex items-center gap-4 flex-wrap">
+          {single.data.movie.cast.map((c) => (
+            <div className="flex flex-col items-center gap-2 ">
+              {
+                <>
+                  <img
+                    className="rounded-full w-16 h-16"
+                    src={c.url_small_image}
+                  />
+                  <p className="text-[#91918d]">{c.name}</p>
+                </>
+              }
+            </div>
+          ))}
+        </div>
+        <div className="flex items-center  gap-3">
+          {single.data.movie.torrents.map((torrent) => (
+            <a
+              className="p-2 bg-[#32323a] transition-all duration-300 rounded-md text-white hover:bg-[#6ac045]"
+              href={torrent.url}
+            >
+              {`${torrent.quality} ${torrent.type}`}
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
